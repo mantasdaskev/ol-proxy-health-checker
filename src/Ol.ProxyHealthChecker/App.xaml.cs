89d18a9d2300fd50fd03b23e2fd869f9bc27981e
mantas.daskevicius <mantas.daskevicius@mediafon.com>
2025-05-23 22:47:32 +0300

    add initial project

---
+++ src/Ol.ProxyHealthChecker/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Ol.ProxyHealthChecker
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
